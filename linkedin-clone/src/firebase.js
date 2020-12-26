283f9963d89c7f698d10077ca65a57712be12457
benjaminhuanghuang <bhuang@rms.com>
2020-12-26 10:46:38 -0800

    Post

--- linkedin-clone/src/firebase.js
+++ linkedin-clone/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: "AIzaSyAhPbdGcRlpLyy8I2HUAtxw51_sEYBBj3k",
   authDomain: "ben-firebase-study.firebaseapp.com",
@@ -7,5 +9,12 @@ const firebaseConfig = {
   messagingSenderId: "502096306530",
   appId: "1:502096306530:web:3254847645f9fdd97a4c3b"
 };
+
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+export const db = firebaseApp.firestore();
+export const auth = firebaseApp.auth();
+
 
 
